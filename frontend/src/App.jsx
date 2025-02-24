a4c9af4a155f2e96da2e5c12a780821dbe0b710a
InduwaraGayashan001 <141438053+InduwaraGayashan001@users.noreply.github.com>
2025-02-24 19:27:28 +0530

    Add Homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import "./layout.scss"
 import Navbar from "./components/navbar/Navbar"
+import HomePage from "./routes/homepage/homePage"
 
 function App() {
   return (
-    <div className= "layout"> 
-      <Navbar/>
+    <div className="layout">
+      <div className="navBar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
+      
     </div>
   )
 }
