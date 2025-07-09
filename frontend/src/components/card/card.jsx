bb416e25a4ab2ff0e697951127fefa2ea1904358
InduwaraGayashan001 <induwaragayashan@gmail.com>
2025-07-09 12:42:20 +0530

    Fix formatting

--- frontend/src/components/card/card.jsx
+++ frontend/src/components/card/card.jsx
@@ -1,19 +1,19 @@
-import './card.scss';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "./card.scss";
 
-function Card({item}) {
+function Card({ item }) {
     return (
         <div className="card">
-            <Link to={`/${item.id}`} className='imageContainer'>
-                <img src={item.images[0]} alt="" />
+            <Link to={`/${item.id}`} className="imageContainer">
+                <img src={item.img[0]} alt="" />
             </Link>
             <div className="textContainer">
                 <h2 className="title">
-                    <Link to={`/${item.id}`} >{ item.title}</Link>
+                    <Link to={`/${item.id}`}>{item.title}</Link>
                 </h2>
                 <p className="address">
                     <img src="/pin.png" alt="" />
-                    <span>{item.address }</span>
+                    <span>{item.address}</span>
                 </p>
                 <p className="price">$ {item.price}</p>
                 <div className="bottom">
@@ -34,11 +34,11 @@ function Card({item}) {
                         <div className="icon">
                             <img src="/chat.png" alt="" />
                         </div>
-                    </div>  
+                    </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Card;
