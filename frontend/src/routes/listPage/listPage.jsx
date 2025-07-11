3b950ba335299e379612048dcd5b9d25842bcbba
InduwaraGayashan001 <induwaragayashan@gmail.com>
2025-07-11 12:44:39 +0530

    Refactor ListPage component to use useLoaderData for fetching posts and improve loading states

--- frontend/src/routes/listPage/listPage.jsx
+++ frontend/src/routes/listPage/listPage.jsx
@@ -1,26 +1,51 @@
-import { listData } from '../../lib/dummydata';
-import './listPage.scss'
-import Filter from '../../components/filter/filter';
-import Card from '../../components/card/card';
-import Map from '../../components/map/map';
+import "./listPage.scss";
+import Filter from "../../components/filter/Filter";
+import Card from "../../components/card/Card";
+import Map from "../../components/map/Map";
+import { Await, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
 
 function ListPage() {
-    const data = listData
+    const data = useLoaderData(); // data = { postResponse: Promise }
+
     return (
         <div className="listPage">
             <div className="listContainer">
                 <div className="wrapper">
                     <Filter />
-                    {data.map(item =>(
-                        <Card key={item.id} item={item} />
-                    ))}
+                    <Suspense fallback={<p>Loading posts...</p>}>
+                        <Await
+                            resolve={data.postResponse}
+                            errorElement={<p>Error loading posts!</p>}
+                        >
+                            {(postResponse) =>
+                                postResponse?.data?.length > 0 ? (
+                                    postResponse.data.map((post) => (
+                                        <Card key={post.id} item={post} />
+                                    ))
+                                ) : (
+                                    <p>No posts found.</p>
+                                )
+                            }
+                        </Await>
+                    </Suspense>
                 </div>
             </div>
+
             <div className="mapContainer">
-                <Map items={data}/>
+                <Suspense fallback={<p>Loading map...</p>}>
+                    <Await
+                        resolve={data.postResponse}
+                        errorElement={<p>Error loading map data!</p>}
+                    >
+                        {(postResponse) => (
+                            <Map items={postResponse?.data ?? []} />
+                        )}
+                    </Await>
+                </Suspense>
             </div>
         </div>
-    )
+    );
 }
 
 export default ListPage;
