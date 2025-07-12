9395905209d64dd25a080b7f5f7951fb32c7d61b
InduwaraGayashan001 <induwaragayashan@gmail.com>
2025-07-12 19:13:48 +0530

    Refactor getPost to return early from JWT verification and improve response handling

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -46,7 +46,7 @@ export const getPost = async (req, res) => {
     const token = req.cookies?.token;
 
     if (token) {
-      jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+      return jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
         if (!err) {
           const saved = await prisma.savedPost.findUnique({
             where: {
@@ -56,16 +56,18 @@ export const getPost = async (req, res) => {
               },
             },
           });
-          res.status(200).json({ ...post, isSaved: saved ? true : false });
+          return res.status(200).json({ ...post, isSaved: saved ? true : false });
         }
       });
     }
-    res.status(200).json({ ...post, isSaved: false });
+
+    return res.status(200).json({ ...post, isSaved: false })
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "Failed to get post" });
+    return res.status(500).json({ message: "Failed to get post" });
   }
 };
+
 
 export const addPost = async (req, res) => {
   const body = req.body;
